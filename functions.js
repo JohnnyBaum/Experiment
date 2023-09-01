e71f2ea055a9d02be549287cb3ef267f41cbdce2
ThalisonAmaral <thalisonamaral2@gmail.com>
2023-09-01 20:47:33 -0300

    Basic functions added

---
+++ functions.js
@@ -0,0 +1,16 @@
+//Classic function syntax 
+function add(num1, num2){
+    return num1 + num2;
+}
+
+//ES6 Syntax
+
+//Multable function
+let subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+//
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
